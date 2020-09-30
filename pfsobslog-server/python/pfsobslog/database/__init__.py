b4472478c39ec03b346b4d5143a0e121a50a13d8
michitaro <michitaro.nike@gmail.com>
2020-09-30 16:24:34 +0900

    setup alembic

--- pfsobslog-server/python/pfsobslog/database/__init__.py
+++ pfsobslog-server/python/pfsobslog/database/__init__.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-url = 'postgresql://postgres:@localhost/opdb2'
+url = 'postgresql://postgres:@localhost/opdb3'
 
 engine = create_engine(url)#, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
