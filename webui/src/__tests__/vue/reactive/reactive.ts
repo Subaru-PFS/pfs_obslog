50a0f394f1e44368c56e84b03b2a63d882b9c62e
michitaro <michitaro.nike@gmail.com>
2021-05-07 07:26:17 +0900

    .

---
+++ webui/src/__tests__/vue/reactive/reactive.ts
@@ -0,0 +1,45 @@
+import { watch } from "@vue/runtime-core"
+import { $reactive } from "~/reactive"
+
+test('$reactive', () => {
+  const cb = jest.fn(() => { })
+
+  const $ = $reactive({
+    x: 2,
+    get y() {
+      cb()
+      return 2 * $.x
+    },
+    set y(value: number) {
+      $.x = value / 2
+    },
+  })
+
+  expect($.x).toBe(2)
+  expect(cb.mock.calls.length).toBe(0)
+  expect($.y).toBe(4)
+  expect(cb.mock.calls.length).toBe(1)
+  expect($.y).toBe(4)
+  expect(cb.mock.calls.length).toBe(1)
+  $.y = 6
+  expect($.x).toBe(3)
+  expect($.y).toBe(6)
+  expect($.y).toBe(6)
+  expect(cb.mock.calls.length).toBe(2)
+})
+
+test('$reactive with watch', () => {
+  const $ = $reactive({
+    x: 1,
+    get y() {
+      return $.x * 2
+    },
+  })
+
+  const cb = jest.fn(() => { })
+  expect(cb.mock.calls.length).toBe(0)
+  watch(() => $.y, cb, { flush: 'sync' })
+  expect(cb.mock.calls.length).toBe(0)
+  $.x = 2
+  expect(cb.mock.calls.length).toBe(1)
+})
---
+++ webui/src/reactive/index.ts
@@ -0,0 +1,14 @@
+import { reactive, computed } from "@vue/reactivity"
+
+export function $reactive<T extends any>(o: T): T {
+  const ds = Object.getOwnPropertyDescriptors(o)
+  // @ts-ignore
+  return reactive(Object.fromEntries(Object.keys(o).map(k => {
+    const d = ds[k]
+    return [
+      k,
+      // @ts-ignore
+      d.get || d.set ? computed({ get: d.get, set: d.set }) : d.value
+    ]
+  })))
+}
