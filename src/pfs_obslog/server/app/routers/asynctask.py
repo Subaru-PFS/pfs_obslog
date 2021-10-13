ade9933a7fe397535e582425cbba514c69428059
michitaro <michitaro.nike@gmail.com>
2021-10-13 16:57:52 +0900

    more safe implementation for asynctask

--- src/pfs_obslog/server/app/routers/asynctask.py
+++ src/pfs_obslog/server/app/routers/asynctask.py
@@ -5,17 +5,15 @@ from typing import Callable, TypeVar, cast
 
 
 class _g:
-    process_pool = cast(ProcessPoolExecutor, None)
     thread_pool = cast(ThreadPoolExecutor, None)
 
 
 def setup_asynctask():
-    _g.process_pool = ProcessPoolExecutor(2)
     _g.thread_pool = ThreadPoolExecutor()
 
 
 def background_process_typeunsafe(f: Callable, args: tuple):
-    return asyncio.wrap_future(_g.process_pool.submit(f, *args))
+    return asyncio.wrap_future(ProcessPoolExecutor(1).submit(f, *args))
 
 
 T = TypeVar('T')
@@ -23,7 +21,7 @@ U = TypeVar('U')
 
 
 def background_process(f: Callable[[T], U], args: T) -> Future[U]:
-    return asyncio.wrap_future(_g.process_pool.submit(f, args))
+    return background_process_typeunsafe(f, (args,))
 
 
 def background_thread_typeunsafe(f: Callable, args: tuple):
